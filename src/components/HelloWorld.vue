f700ae62253ecd7641181e0194c5e14dee6d3c39
JamesC <568345219@qq.com>
2021-08-31 22:11:08 +0800

    hello word ts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="hello">
-    hello word
+    {{msg}}{{red}}
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-  props: {
-    msg: String
-  }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
+import { defineComponent } from 'vue'
+enum colors  {
+  red,
+  green,
+  blue
 }
+export default defineComponent({
+  // 已启用类型推断
+  setup() {
+    let msg = 'hello world'
+    return {
+      msg,
+      red: colors.red
+    }
+  },
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
